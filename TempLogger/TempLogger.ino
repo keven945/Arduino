2004a9968d3849fb6cc0a02b623e768b0f8afb05
Lin Jianjun <jlin@intern.tuwien.ac.at>
2017-04-16 02:20:58 +0200

    'update'

--- TempLogger/TempLogger.ino
+++ TempLogger/TempLogger.ino
@@ -1,32 +1,59 @@
 // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
 #include <Wire.h>
-#include "RTClib.h"
+#include <RTClib.h>
 #include <dht.h>
 #include <EEPROM.h>
 #include <LiquidCrystal.h>
 
-dht DHT;
 #define DHT11_PIN 3
-
-RTC_DS1307 rtc;
-int addr = 0;
 
 String DateAndTime;
 String TempAndHumi;
+
+int addr = 0;
 byte value;
+
+int buttonPin = A0;
+int ledPin = 13;
+int buttonValue = 0;
+
+const int ButtonUp = 1;
+const int ButtonDown = 2;
+const int ButtonLeft = 3;
+const int ButtonRight = 4;
+const int ButtonEnter = 5;
+
+const int BUTTONULOW = 150;
+const int BUTTONUHIGH = 250;
+const int BUTTONDLOW = 300;
+const int BUTTONDHIGH = 400;
+const int BUTTONLLOW = 500;
+const int BUTTONLHIGH = 600;
+const int BUTTONRLOW = 0;
+const int BUTTONRHIGH = 100;
+const int BUTTONELOW = 700;
+const int BUTTONEHIGH = 800;
+
+dht DHT;
+RTC_DS1307 rtc;
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-int buttonPin = A0;    // select the input pin for the potentiometer
-int ledPin = 13;  
-int buttonValue = 0;   // variable to store the value coming from the sensor
+
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+int buttonState; 
+int lastButtonState = LOW;
 
 void setup () {
+
+  pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
-  while (!Serial); // for Leonardo/Micro/Zero
+
+  while (!Serial);
   Serial.begin(9600);
 
-  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  
+
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
@@ -34,105 +61,127 @@ void setup () {
 
   if (!rtc.isrunning()) {
     Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2017, 3, 19,11, 54, 0));
   }
-  
 }
 
 void loop () {
-      //lcd.clear();
-      DateAndTime = String("");
-      DateTime now = rtc.now();
-      
-      DateAndTime += now.year(),
-      DateAndTime += "-";
+  //lcd.clear();
+  DateAndTime = String("");
+  DateTime now = rtc.now();
 
-      DateAndTime += (getDigits(now.month()));
-      DateAndTime += "-";
-      DateAndTime += (getDigits(now.day()));
-      DateAndTime += " ";
-      DateAndTime += (getDigits(now.hour()));
-      DateAndTime += ':';
-      DateAndTime += (getDigits(now.minute()));
-      
-      
-      TempAndHumi = String("T:");
-     
-      int chk = DHT.read11(DHT11_PIN);
- 
-      TempAndHumi += DHT.temperature;
-      TempAndHumi += "  H:";
-      TempAndHumi += DHT.humidity;
-  
-      //char cbuff[32];
-      //stringData.toCharArray(cbuff, 32);
-      
-      
-      lcd.setCursor(0, 0);
-      lcd.print(DateAndTime);
-      
-       /*for (int index = 0 ; index < sizeof(cbuff); index++) {
-        Serial.print("Input");
-        Serial.print(index);
-        Serial.print("   \t");
-        Serial.println(cbuff[index]);
-     
-      }
-      save_string_to_eeprom(cbuff);
-  
-      for (int index = 0 ; index < 100 ; index++) { //EEPROM.length()*
-        value = EEPROM.read(index);
-        Serial.print(index);
-        Serial.print("   \t");
-        Serial.println(value, DEC);
-      }
-  
-      addr = addr + 1;
-      if (addr == EEPROM.length()) {
-       addr = 0;
-      }*/
+  DateAndTime += now.year(),
+  DateAndTime += "-";
 
-    
-       // set the cursor to column 0, line 1
-       // (note: line 1 is the second row, since counting begins with 0):
-       lcd.setCursor(0, 1);
-       // print the number of seconds since reset:
-       lcd.print(TempAndHumi);
+  DateAndTime += (getDigits(now.month()));
+  DateAndTime += "-";
+  DateAndTime += (getDigits(now.day()));
+  DateAndTime += " ";
+  DateAndTime += (getDigits(now.hour()));
+  DateAndTime += ':';
+  DateAndTime += (getDigits(now.minute()));
+
+  TempAndHumi = String("T:");
+  int chk = DHT.read11(DHT11_PIN);
+
+  TempAndHumi += DHT.temperature;
+  TempAndHumi += "  H:";
+  TempAndHumi += DHT.humidity;
+
+  //char cbuff[32];
+  //stringData.toCharArray(cbuff, 32);
 
 
-        // read the value from the sensor:
-      buttonValue = analogRead(buttonPin);
-      lcd.print(buttonValue);
-      digitalWrite(ledPin, HIGH);
-      // stop the program for <sensorValue> milliseconds:
-      delay(buttonValue);
-      // turn the ledPin off:
-      digitalWrite(ledPin, LOW);
-      // stop the program for for <sensorValue> milliseconds:
-      delay(buttonValue);
+  lcd.setCursor(0, 0);
+  lcd.print(DateAndTime);
 
-      //analogWrite(ledPin, brightness);
-    
-      delay(5000);
-}
+  /*for (int index = 0 ; index < sizeof(cbuff); index++) {
+    Serial.print("Input");
+    Serial.print(index);
+    Serial.print("   \t");
+    Serial.println(cbuff[index]);
 
-String getDigits(byte digits){
-    // utility function for digital clock display: prints colon and leading 0
-    String digitStr;
-    if(digits < 10)
-      digitStr += '0';
-    digitStr += digits;   
-    return digitStr;
-}
+    }
+    save_string_to_eeprom(cbuff);
 
-void save_string_to_eeprom(char* stringIn){
-  
-  for(int i = 0; i <strlen(stringIn); i++){
-      EEPROM.update(i, stringIn[i]);
+    for (int index = 0 ; index < 100 ; index++) { //EEPROM.length()*
+    value = EEPROM.read(index);
+    Serial.print(index);
+    Serial.print("   \t");
+    Serial.println(value, DEC);
+    }
+
+    addr = addr + 1;
+    if (addr == EEPROM.length()) {
+    addr = 0;
+    }*/
+
+
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(TempAndHumi);
+
+
+  // read the value from the sensor:
+  buttonValue = analogRead(buttonPin);
+  //buttonValue = digitalRead(buttonPin);
+  //lcd.print(buttonValue);
+  //digitalWrite(ledPin, HIGH);
+  // stop the program for <sensorValue> milliseconds:
+  //delay(buttonValue);
+  // turn the ledPin off:
+  //digitalWrite(ledPin, LOW);
+  // stop the program for for <sensorValue> milliseconds:
+  //Serial.println(buttonValue);
+
+  //analogWrite(ledPin, brightness);
+
+  int tmpButtonState = LOW; 
+
+  if (buttonValue > BUTTONULOW && buttonValue < BUTTONUHIGH) {
+    tmpButtonState = ButtonUp;
+  } else if (buttonValue > BUTTONDLOW && buttonValue < BUTTONDHIGH) {
+    tmpButtonState = ButtonDown;
+  } else if (buttonValue > BUTTONLLOW && buttonValue < BUTTONLHIGH) {
+    tmpButtonState = ButtonLeft;
+  } else if (buttonValue > BUTTONRLOW && buttonValue < BUTTONRHIGH) {
+    tmpButtonState = ButtonRight;
+  } else if (buttonValue > BUTTONELOW && buttonValue < BUTTONEHIGH) {
+    tmpButtonState = ButtonEnter;
   }
 
+   // If the switch changed, due to noise or pressing:
+   if (tmpButtonState != lastButtonState) {
+     // reset the debouncing timer
+     lastDebounceTime = millis();
+   }
+
+   if ((millis() - lastDebounceTime) > debounceDelay && tmpButtonState > 0 ) {
+     // whatever the reading is at, it's been there for longer
+     // than the debounce delay, so take it as the actual current state:
+     buttonState = tmpButtonState;
+     Serial.println(buttonState);
+     delay(500);
+   }
+
+   // save the reading.  Next time through the loop,
+   // it'll be the lastButtonState:
+   lastButtonState = tmpButtonState;
+}
+
+String getDigits(byte digits) {
+  // utility function for digital clock display: prints colon and leading 0
+  String digitStr;
+  if (digits < 10)
+    digitStr += '0';
+  digitStr += digits;
+  return digitStr;
+}
+
+void save_string_to_eeprom(char* stringIn) {
+  for (int i = 0; i < strlen(stringIn); i++) {
+    EEPROM.update(i, stringIn[i]);
+  }
 }
